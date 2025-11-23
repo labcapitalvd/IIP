e6a94c21b6bb75e4f81f8e64d2073c49ce2cef8f
SpanishSyntax <SpanishSyntax@cuspide.club>
2025-11-23 12:54:30 -0500

    entrypoint y persistence a build

--- builder.sh
+++ builder.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-cd ../IIP-Auth && docker compose build
-cd ../IIP-Core && docker compose build
-cd ../IIP-IA-Agent && docker compose build
-cd ../IIP-Stats && docker compose --profile prod build
+NO_CACHE_FLAG="${1:-}"
+cd ../IIP-Persistence && docker compose build $NO_CACHE_FLAG
+cd ../IIP-Auth && docker compose build $NO_CACHE_FLAG
+cd ../IIP-Core && docker compose build $NO_CACHE_FLAG
+cd ../IIP-IA-Agent && docker compose build $NO_CACHE_FLAG
+# cd ../IIP-Stats && docker compose --profile prod build $NO_CACHE_FLAG
