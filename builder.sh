2586dabeb96cdd45eb34b84fe77e0e98dd20ddb4
SpanishSyntax <SpanishSyntax@cuspide.club>
2025-11-23 14:40:22 -0500

    fetch

--- builder.sh
+++ builder.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-
-NO_CACHE_FLAG="${1:-}"
-cd ../IIP-Persistence && docker compose build $NO_CACHE_FLAG
-cd ../IIP-Auth && docker compose build $NO_CACHE_FLAG
-cd ../IIP-Core && docker compose build $NO_CACHE_FLAG
-cd ../IIP-IA-Agent && docker compose build $NO_CACHE_FLAG
-# cd ../IIP-Stats && docker compose --profile prod build $NO_CACHE_FLAG
+cd ../IIP-Persistence && docker compose build "$@"
+cd ../IIP-Auth && docker compose build "$@"
+cd ../IIP-Core && docker compose build "$@"
+cd ../IIP-IA-Agent && docker compose build "$@"
