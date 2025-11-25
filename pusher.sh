356d980f759ae474c4e298577d42741a47e0c0e1
SpanishSyntax <SpanishSyntax@cuspide.club>
2025-11-25 14:05:55 -0500

    Release

--- pusher.sh
+++ pusher.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 docker push labcapital/apps:IIP-Persister
-docker push labcapital/apps:IIP-Auth
 docker push labcapital/apps:IIP-Core
 docker push labcapital/apps:IIP-IA-Agent
 # docker push labcapital/apps:IIP-Stats
