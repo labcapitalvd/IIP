f17a392ae7e843b0fe7ea0f194e08e14280351b9
SpanishSyntax <SpanishSyntax@cuspide.club>
2025-12-07 15:54:33 -0500

    modelos con imports

--- builder.sh
+++ builder.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
-cd ../IIP-Persistence && docker compose build "$@"
-cd ../IIP-Core && docker compose build "$@"
-cd ../IIP-IA-Agent && docker compose build "$@"
-cd ../IIP-Stats && docker compose --profile prod build "$@"
+
+if [[ "$1" == "cachebuster" ]]; then
+    CACHEBUST=$(date +%s)
+    shift
+else
+    CACHEBUST=1
+fi
+
+cd ../IIP-Persistence && docker compose build --build-arg CACHEBUST=$CACHEBUST "$@"
+cd ../IIP-Core && docker compose build --build-arg CACHEBUST=$CACHEBUST "$@"
+cd ../IIP-IA-Agent && docker compose build --build-arg CACHEBUST=$CACHEBUST "$@"
+cd ../IIP-Stats && docker compose --profile prod build --build-arg CACHEBUST=$CACHEBUST "$@"
