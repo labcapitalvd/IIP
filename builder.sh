b3b67ed76672107b22901b830cabbc0bdfbad18e
SpanishSyntax <SpanishSyntax@cuspide.club>
2026-01-21 23:38:48 -0500

    paths de secrets

--- builder.sh
+++ builder.sh
@@ -7,7 +7,7 @@ else
     CACHEBUST=1
 fi
 
-cd ../IIP-Persistence && docker compose build --build-arg CACHEBUST=$CACHEBUST "$@"
-cd ../IIP-Core && docker compose build --build-arg CACHEBUST=$CACHEBUST "$@"
-cd ../IIP-IA-Agent && docker compose build --build-arg CACHEBUST=$CACHEBUST "$@"
-cd ../IIP-Stats && docker compose --profile prod build --build-arg CACHEBUST=$CACHEBUST "$@"
+cd ./IIP-Persistence && docker compose build --build-arg CACHEBUST=$CACHEBUST "$@"
+cd ./IIP-Core && docker compose build --build-arg CACHEBUST=$CACHEBUST "$@"
+cd ./IIP-IA-Agent && docker compose build --build-arg CACHEBUST=$CACHEBUST "$@"
+cd ./IIP-Stats && docker compose --profile prod build --build-arg CACHEBUST=$CACHEBUST "$@"
