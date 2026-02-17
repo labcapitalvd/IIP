54cc6ca4a7fa7e9ff958b9ab0f978034789bee40
SpanishSyntax <SpanishSyntax@cuspide.club>
2026-02-17 16:40:58 -0500

    fetcher tenia el -e y las env vars de cachebuster

--- builder.sh
+++ builder.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
-
-if [[ "$1" == "cachebuster" ]]; then
-    CACHEBUST=$(date +%s)
+if [[ "$1" == "fetcher" ]]; then
+    FETCHER=$(date +%s)
     shift
 else
-    CACHEBUST=1
+    FETCHER=1
 fi
 
-cd ./Persistence && docker compose build --build-arg FETCHER=$FETCHER "$@" --build-arg PACKAGES=$PACKAGES "$@"
-cd ./Core && docker compose build --build-arg FETCHER=$FETCHER "$@" --build-arg PACKAGES=$PACKAGES "$@"
-cd ./IA-Agent && docker compose build --build-arg FETCHER=$FETCHER "$@" --build-arg PACKAGES=$PACKAGES "$@"
-cd ./Stats && docker compose --profile prod build --build-arg FETCHER=$FETCHER "$@" --build-arg PACKAGES=$PACKAGES "$@"
+if [[ "$1" == "packages" ]]; then
+    PACKAGES=$(date +%s)
+    shift
+else
+    PACKAGES=1
+fi
+
+
+cd Persistence && docker compose build --build-arg FETCHER=$FETCHER "$@" --build-arg PACKAGES=$PACKAGES "$@"
+cd Core && docker compose build --build-arg FETCHER=$FETCHER "$@" --build-arg PACKAGES=$PACKAGES "$@"
+cd IA-Agent && docker compose build --build-arg FETCHER=$FETCHER "$@" --build-arg PACKAGES=$PACKAGES "$@"
+cd Stats && docker compose --profile prod build --build-arg FETCHER=$FETCHER "$@" --build-arg PACKAGES=$PACKAGES "$@"
