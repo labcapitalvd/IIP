a1967bc480606b37d3e5226e81844dd804c7633b
SpanishSyntax <SpanishSyntax@cuspide.club>
2026-02-18 23:50:27 -0500

    subshells

--- builder.sh
+++ builder.sh
@@ -13,23 +13,31 @@ else
   PACKAGES=1
 fi
 
+(
 cd Persistence && docker compose build \
   --build-arg FETCHER="$FETCHER" "$@" \
   --build-arg PACKAGES="$PACKAGES" "$@" &&
   docker push labcapital/apps:IIP-Persister
+)
 
+(
 cd Core && docker compose build \
   --build-arg FETCHER="$FETCHER" "$@" \
   --build-arg PACKAGES="$PACKAGES" "$@" &&
   docker push labcapital/apps:IIP-Core
+)
 
+(
 cd IA-Agent && docker compose build \
   --build-arg FETCHER="$FETCHER" "$@" \
   --build-arg PACKAGES="$PACKAGES" "$@" &&
   docker push labcapital/apps:IIP-IA-Agent
+)
 
+(
 cd Stats && docker compose \
   --profile prod build \
   --build-arg FETCHER="$FETCHER" "$@" \
   --build-arg PACKAGES="$PACKAGES" "$@" &&
   docker push labcapital/apps:IIP-Stats
+)
