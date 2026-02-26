b414d303457813a9e88c7bde7036f042015bc9fe
SpanishSyntax <SpanishSyntax@cuspide.club>
2026-02-26 15:36:30 -0500

    init de infra actualizado

--- Core/src/infrastructure/repositories/__init__.py
+++ Core/src/infrastructure/repositories/__init__.py
@@ -1,6 +1,75 @@
-from .actors import ActorRepository,ActorSegmentRepository
+from .actors import ActorRepository, ActorSegmentRepository
+from .forms import (
+    CardTemplateRepository,
+    FieldChoiceRepository,
+    FieldGroupRepository,
+    FieldRepository,
+    FormRepository,
+    InfoRepository,
+    QuestionRepository,
+    SectionRepository,
+    SectionTypeRepository,
+)
+from .grading import CriteriaRepository, GradeRepository, ResultRepository
+from .reference import (
+    FieldTypeRepository,
+    RelationalOperatorRepository,
+    RuleTypeRepository,
+    SectionTypeRepository,
+    SubmissionStatusTypeRepository,
+)
+from .rules import (
+    FieldDependencyRepository,
+    FieldRuleRepository,
+    SectionDependencyRepository,
+)
+from .submissions import (
+    AnswerRepository,
+    CardEntryRepository,
+    SubmissionRepository,
+    UserSubmissionLinkRepository,
+)
 
 __all__ = [
+    #Actors
     "ActorRepository",
     "ActorSegmentRepository",
+    #Forms
+    "CardTemplateRepository",
+    "FieldChoiceRepository",
+    "FieldGroupRepository",
+    "FieldRepository",
+    "FormRepository",
+    "InfoRepository",
+    "QuestionRepository",
+    "SectionRepository",
+    "SectionTypeRepository",
+    #Grading
+    "CriteriaRepository",
+    "GradeRepository",
+    "ResultRepository",
+    #Links
+    "Link",
+    #reference
+    "FieldTypeRepository",
+    "RelationalOperatorRepository",
+    "RuleTypeRepository",
+    "SectionTypeRepository",
+    "SubmissionStatusTypeRepository",
+    #Rules
+    "FieldDependencyRepository",
+    "FieldRuleRepository",
+    "SectionDependencyRepository",
+    #submissions
+    "AnswerRepository",
+    "AnswerBooleanRepository",
+    "AnswerCardEntryRepository",
+    "AnswerDateRepository",
+    "AnswerFileRepository",
+    "AnswerMultiChoiceRepository",
+    "AnswerNumericRepository",
+    "AnswerSingleChoiceRepository",
+    "AnswerTextRepository",
+    "SubmissionRepository"
+
 ]
