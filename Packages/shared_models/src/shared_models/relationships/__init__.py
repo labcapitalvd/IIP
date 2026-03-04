a5aa941c31807af647d1d5d2fa60512daedac2db
SpanishSyntax <SpanishSyntax@cuspide.club>
2026-03-04 15:26:20 -0500

    eliminacion de rels

--- Packages/shared_models/src/shared_models/relationships/__init__.py
+++
@@ -1,6 +0,0 @@
-# Módulo init que carga todos los modelos para simplificar importaciones y prevenir imports circulares.
-from . import audit
-from . import auth
-from . import files
-from . import interactions
-from . import links
