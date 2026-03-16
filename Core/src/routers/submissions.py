eacafcd96dc104a6866e545b3ca900916193eadd
SpanishSyntax <SpanishSyntax@cuspide.club>
2026-03-16 15:59:29 -0500

    MVP de submissions router

---
+++ Core/src/routers/submissions.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Query
+
+from shared_utils import AccessContext
+
+from schemas.results import RequestResult
+
+
+router = APIRouter(tags=["Submissions"], prefix="/submissions")
+
+
+@router.get(
+    "/get/one",
+    response_model_exclude_none=True,
+    operation_id="get_one_results",
+)
+async def get_one_form(
+    query: Annotated[RequestResult, Query()],
+    ctx: AccessContext = Depends(),
+):
+    """Obtiene una edición de índice"""
