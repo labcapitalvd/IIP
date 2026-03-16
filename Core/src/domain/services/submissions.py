aa5ce9bf1a0b8e865e86eabbb4a3e3ecd45a1e61
SpanishSyntax <SpanishSyntax@cuspide.club>
2026-03-16 15:59:08 -0500

    rename de submission

--- Core/src/domain/services/submissions.py
+++ Core/src/domain/services/submissions.py
@@ -20,8 +20,17 @@ from infrastructure.uow import SubmissionUoW
 
 
 class SubmissionService:
-    def _handle_nested_card(self, answer_entity: Answer, data: dict):
-        pass
+    def _handle_nested_card(
+        self,
+        parent_card: AnswerCardEntry,
+        nested_data: List[dict],
+        submission: Submission,
+    ):
+        for item_data in nested_data:
+            child_answer = AnswerFactory.create_answer(data=item_data)
+            child_answer.submission = submission
+            child_answer.card_entry = parent_card
+            submission.answers.append(child_answer)
 
     async def process_submission(
         self,
@@ -36,17 +45,22 @@ class SubmissionService:
 
         # 1. Create the Submission Header
         submission = Submission(
-            user_id=user_id,
+            actor_id=user_id,
             form_id=form_id,
         )
 
         # 3. Process the list of answers recursively
         for data in answers_data:
             answer_entity = AnswerFactory.create_answer(data=data)
+            answer_entity.submission = submission
             submission.answers.append(answer_entity)
 
-            if data.get("type") == "card_loop":
-                self._handle_nested_card(answer_entity, data.get("answers"))
+            if data.get("type") == "card_entry":
+                self._handle_nested_card(
+                    parent_card=answer_entity,  # type: ignore
+                    nested_data=data.get("answers", []),
+                    submission=submission,
+                )
 
         uow.submissions.add(submission)
 
