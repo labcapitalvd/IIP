6d96ce4f776dc98824358a947f2fde74b11517bb
SpanishSyntax <SpanishSyntax@cuspide.club>
2026-03-16 14:50:28 -0500

    factory kwargs

--- Core/src/domain/factories/answer_factory.py
+++ Core/src/domain/factories/answer_factory.py
@@ -1,10 +1,16 @@
+from datetime import date
+from decimal import Decimal
 from models import (
     Answer,
     AnswerBoolean,
     AnswerText,
     AnswerNumeric,
     Submission,
-    AnswerCardEntry, AnswerDate, AnswerFile, AnswerMultiChoice, AnswerSingleChoice,
+    AnswerCardEntry,
+    AnswerDate,
+    AnswerFile,
+    AnswerMultiChoice,
+    AnswerSingleChoice,
 )
 
 
@@ -13,32 +19,50 @@ class AnswerFactory:
     def create_answer(data: dict) -> "Answer":
         field_type = data.get("type")
 
+        base_kwargs = {
+            "field_id": data.get("field_id"),
+        }
+
         if field_type == "boolean":
             return AnswerBoolean(
-                field_id=data.get("field_id"),
-                value=bool(data.get("value")),
-                discriminator="AnswerBoolean",
+                **base_kwargs,
+                value=data.get("value"),
             )
 
         elif field_type == "card_entry":
-            return AnswerCardEntry()
+            return AnswerCardEntry(
+                **base_kwargs,
+                value=bool(data.get("value")),
+            )
 
         elif field_type == "date":
-            return AnswerDate()
+            return AnswerDate(
+                **base_kwargs,
+                value=data.get("value"),
+            )
 
         elif field_type == "file":
-            return AnswerFile()
+            return AnswerFile(
+                **base_kwargs,
+                value=data.get("value"),
+            )
 
         elif field_type == "multichoice":
             return AnswerMultiChoice()
 
         elif field_type == "numeric":
-            return AnswerNumeric()
+            return AnswerNumeric(
+                **base_kwargs,
+                value=data.get("value"),
+            )
 
         elif field_type == "singlechoice":
             return AnswerSingleChoice()
 
         elif field_type == "text":
-            return AnswerText()
+            return AnswerText(
+                **base_kwargs,
+                value=data.get("value"),
+            )
 
         raise ValueError(f"Unknown type: {field_type}")
