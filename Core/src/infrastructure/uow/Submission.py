4298180bd6fa6d68ca0a6d129bfae9146aca09ac
SpanishSyntax <SpanishSyntax@cuspide.club>
2026-03-16 11:04:29 -0500

    cleanup of submission repositories

--- Core/src/infrastructure/uow/Submission.py
+++ Core/src/infrastructure/uow/Submission.py
@@ -25,14 +25,6 @@ class SubmissionUoW(UnitOfWork):
 
     submissions: SubmissionRepository
     answers: AnswerRepository
-    answers_boolean: AnswerBooleanRepository
-    answers_card_entry: AnswerCardEntryRepository
-    answers_date: AnswerDateRepository
-    answers_file: AnswerFileRepository
-    answers_multi_choice: AnswerMultiChoiceRepository
-    answers_numeric: AnswerNumericRepository
-    answers_single_choice: AnswerSingleChoiceRepository
-    answers_text: AnswerTextRepository
 
     user_links: UserSubmissionLinkRepository
     multichoice_links: MultiChoiceOptionLinkRepository
@@ -46,14 +38,6 @@ class SubmissionUoW(UnitOfWork):
 
         self.submissions = SubmissionRepository(self.session)
         self.answers = AnswerRepository(self.session)
-        self.answers_boolean = AnswerBooleanRepository(self.session)
-        self.answers_card_entry = AnswerCardEntryRepository(self.session)
-        self.answers_date = AnswerDateRepository(self.session)
-        self.answers_file = AnswerFileRepository(self.session)
-        self.answers_multi_choice = AnswerMultiChoiceRepository(self.session)
-        self.answers_numeric = AnswerNumericRepository(self.session)
-        self.answers_single_choice = AnswerSingleChoiceRepository(self.session)
-        self.answers_text = AnswerTextRepository(self.session)
 
         self.user_links = UserSubmissionLinkRepository(self.session)
         self.multichoice_links = MultiChoiceOptionLinkRepository(self.session)
