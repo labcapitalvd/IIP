99497aaadab4eccaa95cc06bc1e0a5ae4030d4c9
SpanishSyntax <spanishsyntax@pyritelabs.cc>
2026-07-22 10:27:28 -0500

    entry no reqs

--- Core/entrypoint.sh
+++ Core/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 set -e
 
-# CORE PORT
-: "${PORT_CORE:?Environment variable PORT_CORE is required}"
-
 # If validation passes, run the CMD
 exec "$@"
--- Persistence/entrypoint.sh
+++ Persistence/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 set -e
 
-# AUTH PORT
-: "${PORT_AUTH:?Environment variable PORT_AUTH is required}"
-# CORE PORT
-: "${PORT_CORE:?Environment variable PORT_CORE is required}"
-
 # If validation passes, run the CMD
 exec "$@"
