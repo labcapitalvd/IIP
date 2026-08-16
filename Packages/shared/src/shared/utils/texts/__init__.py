9f1f8ee4d5fbb86d68bf96d48319748dffbb9d3c
SpanishSyntax <spanishsyntax@pyritelabs.cc>
2026-08-15 23:45:33 -0500

    format banner returns str

--- Packages/shared/src/shared/utils/texts/__init__.py
+++ Packages/shared/src/shared/utils/texts/__init__.py
@@ -1,8 +1,8 @@
-from .texts import sanitize_text, sanitize_email, print_list, print_banner
+from .texts import sanitize_text, sanitize_email, format_list, format_banner
 
 __all__ = [
     "sanitize_text",
     "sanitize_email",
-    "print_list",
-    "print_banner",
+    "format_list",
+    "format_banner",
 ]
